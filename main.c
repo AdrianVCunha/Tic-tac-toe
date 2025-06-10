071af20bc9f37a98d5204e5f508c24b4484631c4
Adrian <cunhaadrian128@gmail.com>
2025-06-10 09:08:32 -0300

    Implementation of showBoard and makeMove functions, and beginning of checkMove

--- main.c
+++ main.c
@@ -1,49 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Cabeçalho das funções secundarias:
-void MostrarBoasVindas();
-char CriaTabuleiro(char tabuleiro[3][3]);
+// Function headers
+void ShowWelcomeMessage();
+void CreateBoard(char board[3][3]);
+void showBoard(char board[3][3]);
+void checkMove(char board[3][3], char *move);
+void makeMove(char board[3][3], char move, int player);
 
-
-//Função principal 
-int main(){
-
-	char tabuleiro[3][3];
-	MostrarBoasVindas();
-	tabuleiro[3][3] = CriaTabuleiro(tabuleiro);
-
+// Main function
+int main() {
+	char board[3][3];
+	ShowWelcomeMessage();
+	CreateBoard(board);
 	
+	int option = 1;	
+	do{
+		char move;
+		printf("Player 1: ");
+		scanf(" %c",&move);
+		checkMove(board,&move);
+		makeMove(board, move, 1);	
+		showBoard(board);
+		
+		printf("Player 2: ");
+		scanf(" %c",&move);
+		checkMove(board,&move);
+		makeMove(board, move, 2);
+		showBoard(board);
+
+										
+	}while (option == 1);
+	
+	
+	return 0;
 }
 
-char CriaTabuleiro(char tabuleiro[3][3]){
-
+// Function to create and display the board
+void CreateBoard(char board[3][3]) {
 	char x = '1';
-	for(int i = 0; i<3; i++){
-		for(int j = 0; j<3; j++){
-			tabuleiro[i][j] =x++;
-
+	for(int i = 0; i < 3; i++) {
+		for(int j = 0; j < 3; j++) {
+			board[i][j] = x++;
 		}
 	}
 	
-	for (int i = 0; i < 3; i++) {
-        	printf(" %c | %c | %c ", tabuleiro[i][0], tabuleiro[i][1], tabuleiro[i][2]);
-        	if (i < 2) {
-            	printf("\n---|---|---\n");
-        }
-    }
-		
+	showBoard(board);
 }
-void MostrarBoasVindas() {
-    printf("=======================================\n");
-    printf("       BEM-VINDO AO JOGO DA VELHA      \n");
-    printf("=======================================\n\n");
 
-    printf("Instrucoes:\n");
-    printf("- Dois jogadores se alternam (X e O).\n");
-    printf("- Para jogar, digite o número da casa desejada.\n");
-    printf("- Vence quem alinhar 3 símbolos na horizontal,\n");
-    printf("  vertical ou diagonal.\n\n");
+// Function to show welcome message
+void ShowWelcomeMessage() {
+	printf("=======================================\n");
+	printf("       WELCOME TO TIC-TAC-TOE          \n");
+	printf("=======================================\n\n");
 
-    printf("Boa sorte e divirta-se!\n\n");
+	printf("Instructions:\n");
+	printf("- Two players take turns (X and O).\n");
+	printf("- To play, enter the number of the desired space.\n");
+	printf("- The winner is the one who aligns 3 symbols \n");
+	printf("  horizontally, vertically, or diagonally.\n\n");
+
+	printf("Good luck and have fun!\n\n");
+}
+//Function to show board
+void showBoard(char board[3][3]){
+	printf("\n");
+	for (int i = 0; i < 3; i++) {
+		printf(" %c | %c | %c \n", board[i][0], board[i][1], board[i][2]);
+		if (i < 2) {
+			printf("---|---|---\n");
+		}
+	}
+	printf("\n");
+}
+//Function to check if the move is valid
+void checkMove(char board[3][3], char *move){
+
+	if(*move<='0' || *move>'9'){
+		printf("The move is invalid, please try again:\n");
+		printf("New move: ");
+		scanf(" %c", move);
+		checkMove(board, move);
+	}
+	else{
+		return;
+	}
+}
+//Function to make the move
+void makeMove(char board[3][3], char move, int player){
+	for(int i = 0; i<3; i++){
+		for(int j =0; j<3; j++){
+			if(board[i][j]==move){
+				if(player == 1){
+					board[i][j] = 'X';
+				}  
+				if(player == 2){
+					board[i][j] = 'O';
+				}
+			}
+		}
+	}
+	
 }
