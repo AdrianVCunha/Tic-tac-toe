368208953b74e374d57b9cc6531da42aec2bef56
Adrian <cunhaadrian128@gmail.com>
2025-06-14 15:52:16 -0300

    Add row, column, and diagonal win checks

--- main.c
+++ main.c
@@ -7,30 +7,65 @@ void CreateBoard(char board[3][3]);
 void showBoard(char board[3][3]);
 void checkMove(char board[3][3], char *move);
 void makeMove(char board[3][3], char move, int player);
+int checkWinner(char board[3][3], int moves, int player);
+int checkDiagonalsWin(char board[3][3]);
+int checkRowWin(char board[3][3]);
+int checkCollumnWin(char board[3][3]);
+void showWinnerScreen(int player, char board[3][3]);
+void showDrawScreen(char board[3][3]);
+
 
 // Main function
 int main() {
 	char board[3][3];
+	int moves = 1;
 	ShowWelcomeMessage();
 	CreateBoard(board);
 	
-	int option = 1;	
+	int loop = 1;	
 	do{
 		char move;
+		int winner = 0;
 		printf("Player 1: ");
 		scanf(" %c",&move);
 		checkMove(board,&move);
-		makeMove(board, move, 1);	
+		makeMove(board, move, 1);
+		moves++;
 		showBoard(board);
+		winner = checkWinner(board, moves, 1);
+		if(winner!=0){
+			if(winner!=3){
+				showWinnerScreen(winner, board);
+				break;
+			}
+			else{
+				showDrawScreen(board);
+				break;
+			}
+		}
 		
 		printf("Player 2: ");
 		scanf(" %c",&move);
 		checkMove(board,&move);
 		makeMove(board, move, 2);
+		moves++;
 		showBoard(board);
+		winner = checkWinner(board, moves, 1);
+		if(winner!=0){
+			if(winner!=3){
+				showWinnerScreen(winner, board);
+				break;
+			}
+			else{
+				showDrawScreen(board);
+				break;
+			}
+			
+		}
 
 										
-	}while (option == 1);
+	}while (loop == 1);
+	printf("Thanks for play");
 	
 	
 	return 0;
@@ -75,15 +110,17 @@ void showBoard(char board[3][3]){
 }
 //Function to check if the move is valid
 void checkMove(char board[3][3], char *move){
-
-	if(*move<='0' || *move>'9'){
-		printf("The move is invalid, please try again:\n");
-		printf("New move: ");
-		scanf(" %c", move);
-		checkMove(board, move);
+	int loop = 1;
+	if(*move<='0'){
+		while(loop = 1){
+			printf("The move is invalid, please try again:\n");
+			printf("New move: ");
+			scanf(" %c", move);
+			checkMove(board, move);	
+			break;
+		}	
 	}
-	else{
-		return;
+	else if(move){
 	}
 }
 //Function to make the move
@@ -101,4 +138,126 @@ void makeMove(char board[3][3], char move, int player){
 		}
 	}
 	
+}
+//Function to check if have winner
+int checkWinner(char board[3][3], int moves, int player){
+	int winner = 0;
+
+	if(moves>=5){
+		winner = checkRowWin(board);
+		if(winner == 0){
+			winner = checkDiagonalsWin(board);
+			if(winner == 0){
+				winner = checkCollumnWin(board);
+			}
+		}
+		else if(winner == 0 && moves == 9){
+			winner = 3;
+		}
+	}		
+	
+	
+	
+	return winner;
+	
+}
+//Function to check if the diagonals are equals
+int checkDiagonalsWin(char board[3][3]){
+
+	if (board[0][0] == board[1][1] && board[1][1] == board[2][2]){
+        	if (board[0][0] == 'X') {
+            	return 1;
+        	}
+			else if(board[0][0] == 'O'){
+				return 2;
+			}
+		}
+    
+	if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+        if (board[0][2] == 'X') {
+           		return 1;
+        }
+		else if (board[0][2] == 'O') {
+           		return 2;
+        }
+    }
+	
+	return 0;
+}
+//Function to check if the rows are equal
+int checkRowWin(char board[3][3]){
+	
+		if(board[0][0] == board[0][1] && board[0][1] == board[0][2]){
+			if(board[0][0] == 'X'){
+				return 1;
+			}
+			else if(board[0][0] == 'O'){
+				return 2;
+			}
+		}
+		else if(board[1][0] == board[1][1] && board[1][1] == board[1][2]){
+			if(board[1][0] == 'X'){
+				return 1;
+			}
+			else if(board[1][0] == 'O'){
+				 return 2;
+			}
+		}
+		else if(board[2][0] == board[2][1] && board[2][1] == board[2][2]){
+			if(board[2][0] == 'X'){
+				return 1;
+			}
+			else if(board[2][0] == 'O'){
+				return 2;
+			}
+		}
+
+		return 0;
+}
+//Function to check if the colluns are equal
+int checkCollumnWin(char board[3][3]){
+	if(board[0][0] == board[1][0] && board[1][0] == board[2][0]){
+			if(board[0][0] == 'X'){
+				return 1;
+			}
+			else if(board[0][0] == 'O'){
+				return 2;
+			}
+		}
+		else if(board[0][1] == board[1][1] && board[1][1] == board[2][1]){
+			if(board[0][1] == 'X'){
+				return 1;
+			}
+			else if(board[0][1] == 'O'){
+				 return 2;
+			}
+		}
+		else if(board[0][2] == board[1][2] && board[1][2] == board[2][2]){
+			if(board[0][2] == 'X'){
+				return 1;
+			}
+			else if(board[0][2] == 'O'){
+				return 2;
+			}
+		}
+		return 0;
+}
+//Function to congratulations
+void showWinnerScreen(int player, char board[3][3]) {
+    printf("\n==============================\n");
+    printf("      CONGRATULATIONS!  \n");
+    printf("==============================\n");
+    printf("       Player %d wins!       \n", player);
+    printf("==============================\n\n");
+	showBoard(board);
+	
+}
+//Function to draw
+void showDrawScreen(char board[3][3]) {
+    printf("\n==============================\n");
+    printf("        It's a draw!         \n");
+    printf("==============================\n");
+    printf("  Nobody wins this time...   \n");
+    printf("==============================\n\n");
+	showBoard(board);
 }
